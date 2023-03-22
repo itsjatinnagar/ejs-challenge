0c07807d7c23818a47ab50ebe0488b267d2abc27
Jatin Nagar <121741542+itsjatinnagar@users.noreply.github.com>
2023-03-22 20:57:22 +0530

    Complete : Challenge-12

--- app.js
+++ app.js
@@ -19,8 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-    res.render("home", { content: homeStartingContent });
-    console.log(POSTS);
+    res.render("home", { content: homeStartingContent, posts: POSTS });
 });
 
 app.get("/about", function (req, res) {
